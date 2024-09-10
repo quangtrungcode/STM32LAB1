4be42d28de55af9f1629c3fbbc3525d506d29857
quangtrung24122002 <quangtrung24122002@gmail.com>
2024-09-10 22:03:18 +0700

    kjhb

--- STM32Project/Core/Src/Ex3.c
+++ STM32Project/Core/Src/Ex3.c
@@ -18,7 +18,7 @@
   */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
-#include "main.h"
+#include <Ex3.h>
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
