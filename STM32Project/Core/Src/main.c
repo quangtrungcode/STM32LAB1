fc7404e9022f476210fb4ab902acdfbe1aa069b1
quangtrung24122002 <quangtrung24122002@gmail.com>
2024-09-10 22:31:18 +0700

    vvv

--- STM32Project/Core/Src/main.c
+++ STM32Project/Core/Src/main.c
@@ -18,7 +18,7 @@
   */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
-#include <Ex1.h>
+#include <main.h>
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
