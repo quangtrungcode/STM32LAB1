656f624522f62e80143b362acabdae1c5974b937
quangtrung24122002 <quangtrung24122002@gmail.com>
2024-09-10 22:32:55 +0700

    khi

--- STM32Project/Core/Src/main.c
+++ STM32Project/Core/Src/main.c
@@ -18,7 +18,7 @@
   */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
-#include <Ex3.h>
+#include <main.h>
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
