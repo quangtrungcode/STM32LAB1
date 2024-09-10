c7e563e7cdf056045524cdedcb2a415fdcccd040
quangtrung24122002 <quangtrung24122002@gmail.com>
2024-09-10 22:02:04 +0700

    ggg

--- STM32Project/Core/Src/Ex2.c
+++ STM32Project/Core/Src/Ex2.c
@@ -18,7 +18,7 @@
   */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
-#include "main.h"
+#include <Ex2.h>
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
