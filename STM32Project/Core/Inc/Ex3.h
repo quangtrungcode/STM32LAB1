8bc96788c84a55129e115c1d0e8c57295961c0d1
quangtrung24122002 <quangtrung24122002@gmail.com>
2024-09-13 16:11:08 +0700

    vvvv

--- STM32Project/Core/Inc/Ex3.h
+++ STM32Project/Core/Inc/Ex3.h
@@ -11,4 +11,5 @@
 
 
 #endif /* INC_EX3_H_ */
+#include "stm32f1xx_hal.h"
 void settrafficlight1(GPIO_TypeDef* LED_GIPO_Port, uint16_t LED_Pin,GPIO_TypeDef* LED_1GIPO_Port, uint16_t LED_1Pin,GPIO_TypeDef* LED_2GIPO_Port, uint16_t LED_2Pin);
