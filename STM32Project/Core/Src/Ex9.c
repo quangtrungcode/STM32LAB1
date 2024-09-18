5adca8f405b0f7fde31eec56c87f5da132cd1537
quangtrungcode <quangtrung24122002@gmail.com>
2024-09-18 09:04:36 +0700

    update

--- STM32Project/Core/Src/Ex9.c
+++ STM32Project/Core/Src/Ex9.c
@@ -12,6 +12,7 @@ uint16_t segmentPins[12]={LED0_Pin,LED1_Pin,LED2_Pin,LED3_Pin,LED4_Pin,LED5_Pin,
       	   ,LED8_Pin,LED9_Pin,LED10_Pin,LED11_Pin};
 void clearNumberOnClock(int num) {
           	 HAL_GPIO_WritePin(segmentPorts[num], segmentPins[num], SET);
+          	 HAL_GPIO_WritePin(GPIOA, LED_PINS & ~ segmentPins[num], RESET);
         }
 void Ex9_run(){
 	if(counter>=12) counter=0;
